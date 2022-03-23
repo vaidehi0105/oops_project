4a4be2507acfcb5ba9834bd8c9873bb57e3ffed2
Vaidehi Bedekar <72137592+vaidehi0105@users.noreply.github.com>
2022-03-23 08:41:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function togglePopup1(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+function togglePopup2(){
+    document.getElementById("popup-2").classList.toggle("active");
+}
+function togglePopup3(){
+    document.getElementById("popup-3").classList.toggle("active");
+}
+function togglePopup4(){
+    document.getElementById("popup-4").classList.toggle("active");
+}
+function togglePopup5(){
+    document.getElementById("popup-5").classList.toggle("active");
+}
+function togglePopup6(){
+    document.getElementById("popup-6").classList.toggle("active");
+}
+function togglePopup7(){
+    document.getElementById("popup-7").classList.toggle("active");
+}
+function togglePopup8(){
+    document.getElementById("popup-8").classList.toggle("active");
+}
+function togglePopup9(){
+    document.getElementById("popup-9").classList.toggle("active");
+}
+function togglePopup10(){
+    document.getElementById("popup-10").classList.toggle("active");
+}
+function togglePopup11(){
+    document.getElementById("popup-11").classList.toggle("active");
+}
+function togglePopup12(){
+    document.getElementById("popup-12").classList.toggle("active");
+}
+function togglePopup13(){
+    document.getElementById("popup-13").classList.toggle("active");
+}
+function togglePopup14(){
+    document.getElementById("popup-14").classList.toggle("active");
+}
+function togglePopup15(){
+    document.getElementById("popup-15").classList.toggle("active");
+}
+function togglePopup16(){
+    document.getElementById("popup-16").classList.toggle("active");
+}
+function togglePopup17(){
+    document.getElementById("popup-17").classList.toggle("active");
+}
